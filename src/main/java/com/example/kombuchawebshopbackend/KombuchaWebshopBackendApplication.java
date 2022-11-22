2fda62826973330dd9b86207708ff092c4de2d27
Dániel Szabó <60754393+KingAlistair@users.noreply.github.com>
2022-11-22 09:19:02 +0100

    Add files via upload

---
+++ src/main/java/com/example/kombuchawebshopbackend/KombuchaWebshopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.kombuchawebshopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KombuchaWebshopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KombuchaWebshopBackendApplication.class, args);
+    }
+
+}
