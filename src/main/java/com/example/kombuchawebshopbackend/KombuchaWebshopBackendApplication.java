1682e028efef69afbda180dff914110e9c08f453
Dániel Szabó <kingalistairlol@gmail.com>
2022-12-13 10:51:21 +0100

    Blog Entity Date and Time seperated, changed InitialData a bit

--- src/main/java/com/example/kombuchawebshopbackend/KombuchaWebshopBackendApplication.java
+++ src/main/java/com/example/kombuchawebshopbackend/KombuchaWebshopBackendApplication.java
@@ -9,5 +9,4 @@ public class KombuchaWebshopBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(KombuchaWebshopBackendApplication.class, args);
     }
-
 }
